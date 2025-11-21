dae1527b1d377e6958894a011ce1777a0c3639a0
HadriX5 <adriasuarezbatllori@gmail.com>
2025-11-21 15:45:31 +0100

    encoding fixes and data cleaning function update

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,1 @@
-from .data_processing import load_raw_data, clean_data
+from .data_processing import load_raw_data, clean_data, repair_encoding
--- src/data_processing/__init__.py
+++ src/data_processing/__init__.py
@@ -1,2 +1,3 @@
 from .loader import load_raw_data
 from .preprocessing import clean_data
+from .repair_encoding import repair_encoding
